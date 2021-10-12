0ba3d498a0e88d2375ed830d9aeeb73c35caa9ae
MichelleStella <82376470+MichelleStella@users.noreply.github.com>
2021-10-12 17:38:07 +0200

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
+    input("Greetings ! How are you doing ?\n")
+    input("Oh, how interesting. Tell me more !\n")
+    print("Thanks for sharing!")
 
 if __name__ == '__main__':
     main()
